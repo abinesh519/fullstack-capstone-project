02f74162dddecc28e0e7c2f3962972ecfabe6eae
abinesh519 <abinesh519@gmail.com>
2025-12-01 22:09:50 -0500

    initial commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 //{{insert code here}}
 const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
 // const searchRoutes = require('./routes/searchRoutes');
@@ -40,6 +41,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/auth', authRoutes);
 // app.use('/api/search', searchRoutes);
 app.use('/api/search', searchRoutes);
 
