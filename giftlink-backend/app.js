8db6907b0f56500557c635bcc2a4dd4d0d76ef68
abinesh519 <abinesh519@gmail.com>
2025-12-02 11:32:41 -0500

    initial commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -67,6 +67,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 function App() {
   const navigate = useNavigate();
   return (
@@ -78,6 +79,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
